b20e76aea8273242e69e28ff5716726a703e0a7a
Zach <zawood11@gmail.com>
2022-03-08 16:27:52 -0500

    commit tree c6e25e0d89a8f2decf21cdf53f103991c097004b

---
+++ db/migrate/20220308211400_create_costumes.rb
@@ -0,0 +1,11 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps null: false
+    end
+  end
+end
